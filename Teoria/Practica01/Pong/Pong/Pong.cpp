1c53228e0274f609927035a17cb1b122035484f2
Robinlop64 <robin.rubiks@gmail.com>
2026-03-10 14:18:02 -0600

    Pong marcador y deformacion

--- Teoria/Practica01/Pong/Pong/Pong.cpp
+++ Teoria/Practica01/Pong/Pong/Pong.cpp
@@ -1,34 +1,42 @@
 #include <windows.h>
 #include <GL/glut.h>
 #include <math.h>
+#include <string>
 
-#define PI 3.1415926535898
+
+#define PI 3.1415926535898 
 
 int width = 900, height = 500;
 int interval = 16;
 
-// Raquetas
+int scoreLeft = 0, scoreRight = 0;
+
 int raqW = 20;
 int raqH = 150;
-int raqSp = 6;
+int raqSp = 4;
 
 float raqLeftX = 10.0f;
 float raqLeftY = 240.0f;
 
-float raqRightX = 0.0f; 
+float raqRightX = width - raqW - 10;
 float raqRightY = 240.0f;
 
-// Teclas
 bool keyLeft[256] = { false };
 bool keyRight[256] = { false };
 
-// Bola
 double xpos = width / 2;
 double ypos = height / 2;
-float xdir = 1.0f;
-float ydir = -0.6f;
-float ballSp = 8.0f;
+
+float ydir = -1.0f;
+float xdir = (rand() % 2 == 0) ? -1.0f : 1.0f;
+
 int ballSize = 8;
+
+double sx = 1.0, sy = 1.0, squash = 0.8;
+
+float ballSp = 10.0f;
+float speedIncr = 0.2f;
+float maxSp = 25.0f;
 
 GLint circle_points = 100;
 
@@ -41,26 +49,42 @@ void MyCircle2f(GLfloat centerx, GLfloat centery, GLfloat radius) {
     glEnd();
 }
 
-void displayRaq(float x, float y, float w, float h) {
+void reshape(int width, int height) {
+    glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0, width, 0.0, height);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+void displayText(float x, float y, std::string text) {
+    glRasterPos2f(x, y);
+    for (size_t i = 0; i < text.size(); ++i) {
+        glutBitmapCharacter(GLUT_BITMAP_8_BY_13, text[i]);
+    }
+}
+
+void displayRaq(float x, float y, float width, float height) {
     glBegin(GL_QUADS);
     glVertex2f(x, y);
-    glVertex2f(x + w, y);
-    glVertex2f(x + w, y + h);
-    glVertex2f(x, y + h);
+    glVertex2f(x + width, y);
+    glVertex2f(x + width, y + height);
+    glVertex2f(x, y + height);
     glEnd();
 }
 
-void reshape(int w, int h) {
-    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0.0, w, 0.0, h);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+void keyPressLeft(unsigned char key, int x, int y) { keyLeft[key] = true; }
+void keyPressDropLeft(unsigned char key, int x, int y) { keyLeft[key] = false; }
 
-    width = w;
-    height = h;
-    raqRightX = width - raqW - 10;
+void keyPressRight(int key, int x, int y) { keyRight[key] = true; }
+void keyPressDropRight(int key, int x, int y) { keyRight[key] = false; }
+
+void keyboard() {
+    glutKeyboardFunc(keyPressLeft);
+    glutKeyboardUpFunc(keyPressDropLeft);
+    glutSpecialFunc(keyPressRight);
+    glutSpecialUpFunc(keyPressDropRight);
 }
 
 void vec2_norm(float& x, float& y) {
@@ -72,86 +96,93 @@ void vec2_norm(float& x, float& y) {
     }
 }
 
-// Input: izquierda 
-void keyDown(unsigned char key, int, int) { keyLeft[key] = true; }
-void keyUp(unsigned char key, int, int) { keyLeft[key] = false; }
+void updateBall() {
 
-// Input: derecha 
-void specialDown(int key, int, int) { keyRight[key] = true; }
-void specialUp(int key, int, int) { keyRight[key] = false; }
+    xpos += xdir * ballSp;
+    ypos += ydir * ballSp;
 
-void keyboard() {
-    glutKeyboardFunc(keyDown);
-    glutKeyboardUpFunc(keyUp);
-    glutSpecialFunc(specialDown);
-    glutSpecialUpFunc(specialUp);
-}
+    if (xpos < raqLeftX + raqW && xpos > raqLeftX && ypos < raqLeftY + raqH && ypos > raqLeftY) {
 
-void resetBall(int direction) {
-    xpos = width / 2;
-    ypos = height / 2;
+        float t = ((ypos - raqLeftY) / raqH) - 0.5f;
+        xdir = fabs(xdir);
 
-    xdir = (direction >= 0) ? 1.0f : -1.0f;
-    ydir = (rand() % 2 == 0) ? 0.6f : -0.6f;
+        ydir = t * 1.5f + ((rand() % 10 - 5) / 20.0f);
+
+        ballSp = fmin(ballSp + speedIncr, maxSp);
+
+        sx = squash;
+        sy = 1.2;
+    }
+
+    if (xpos > raqRightX && xpos < raqRightX + raqW && ypos < raqRightY + raqH && ypos > raqRightY) {
+
+        float t = ((ypos - raqRightY) / raqH) - 0.5f;
+        xdir = -fabs(xdir);
+
+        ydir = t * 1.5f + ((rand() % 10 - 5) / 20.0f);
+
+        ballSp = fmin(ballSp + speedIncr, maxSp);
+
+        sx = squash;
+        sy = 1.2;
+    }
+
+    if (xpos < 0) {
+        ++scoreRight;
+
+        xpos = width / 2;
+        ypos = height / 2;
+
+        xdir = fabs(xdir);
+        ydir = 0;
+
+        sx = sy = 1.0;
+        ballSp = 10.0f;
+    }
+
+    if (xpos > width) {
+        ++scoreLeft;
+
+        xpos = width / 2;
+        ypos = height / 2;
+
+        xdir = -fabs(xdir);
+        ydir = 0;
+
+        sx = sy = 1.0;
+        ballSp = 10.0f;
+    }
+
+    if (ypos > height) {
+        ydir = -fabs(ydir);
+
+        sx = 1.2;
+        sy = squash;
+    }
+
+    if (ypos < 0) {
+        ydir = fabs(ydir);
+
+        sx = 1.2;
+        sy = squash;
+    }
 
     vec2_norm(xdir, ydir);
 }
 
-void updateBall() {
-    xpos += xdir * ballSp;
-    ypos += ydir * ballSp;
+void update(int value) {
 
-    // Rebote paredes superior/inferior
-    if (ypos > height) {
-        ypos = height;
-        ydir = -fabs(ydir);
-    }
-    if (ypos < 0) {
-        ypos = 0;
-        ydir = fabs(ydir);
-    }
+    if (keyLeft['w'] && raqLeftY + raqH < height)
+        raqLeftY += raqSp;
 
-    // Colisión raqueta izquierda 
-    if (xpos - ballSize < raqLeftX + raqW &&
-        xpos + ballSize > raqLeftX &&
-        ypos + ballSize > raqLeftY &&
-        ypos - ballSize < raqLeftY + raqH) {
+    if (keyLeft['s'] && raqLeftY > 0)
+        raqLeftY -= raqSp;
 
-        float t = ((ypos - raqLeftY) / raqH) - 0.5f;
-        xdir = fabs(xdir);
-        ydir = t * 1.5f;
-        vec2_norm(xdir, ydir);
+    if (keyRight[GLUT_KEY_UP] && raqRightY + raqH < height)
+        raqRightY += raqSp;
 
-        xpos = raqLeftX + raqW + ballSize; 
-    }
-
-    // Colisión raqueta derecha
-    if (xpos + ballSize > raqRightX &&
-        xpos - ballSize < raqRightX + raqW &&
-        ypos + ballSize > raqRightY &&
-        ypos - ballSize < raqRightY + raqH) {
-
-        float t = ((ypos - raqRightY) / raqH) - 0.5f;
-        xdir = -fabs(xdir);
-        ydir = t * 1.5f;
-        vec2_norm(xdir, ydir);
-
-        xpos = raqRightX - ballSize;
-    }
-
-
-    if (xpos < 0) resetBall(+1);
-    if (xpos > width) resetBall(-1);
-}
-
-void update(int) {
-    // Raqueta izquierda
-    if (keyLeft['w'] && raqLeftY + raqH < height) raqLeftY += raqSp;
-    if (keyLeft['s'] && raqLeftY > 0) raqLeftY -= raqSp;
-
-    // Raqueta derecha
-    if (keyRight[GLUT_KEY_UP] && raqRightY + raqH < height) raqRightY += raqSp;
-    if (keyRight[GLUT_KEY_DOWN] && raqRightY > 0) raqRightY -= raqSp;
+    if (keyRight[GLUT_KEY_DOWN] && raqRightY > 0)
+        raqRightY -= raqSp;
 
     updateBall();
 
@@ -160,39 +191,47 @@ void update(int) {
 }
 
 void display() {
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
-    // Raquetas
     displayRaq(raqLeftX, raqLeftY, raqW, raqH);
     displayRaq(raqRightX, raqRightY, raqW, raqH);
 
-    // Bola
+    displayText(width / 2 - 10, height - 15,
+        std::to_string(scoreLeft) + ":" + std::to_string(scoreRight));
+
     glPushMatrix();
-    glTranslatef((float)xpos, (float)ypos, 0.0f);
-    MyCircle2f(0, 0, (float)ballSize);
+
+    glTranslatef(xpos, ypos, 0);
+    glScalef(sx, sy, 1.0);
+
+    MyCircle2f(0, 0, ballSize);
+
     glPopMatrix();
 
     glutSwapBuffers();
 }
 
 int main(int argc, char* argv[]) {
+
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(width, height);
-    glutCreateWindow("Pong - Version 2");
 
-    raqRightX = width - raqW - 10;
+    glutInitWindowSize(width, height);
+    glutCreateWindow("Pong - Atari");
 
     glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
+
     keyboard();
 
     glutTimerFunc(interval, update, 0);
 
+    glutReshapeFunc(reshape);
+
     glColor3f(1.0f, 1.0f, 1.0f);
-    resetBall((rand() % 2 == 0) ? 1 : -1);
 
     glutMainLoop();
-    return 0;
+
+    return 1;
 }
